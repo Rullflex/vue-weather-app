6ce09942089a2e9d28443d4093e250adf84c814b
Dmitry <libafer99@gmail.com>
2021-05-20 11:40:22 +0500

    https fix

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ export const celsiusToFahrenheit = celsius => (celsius * 9/5) + 32
 
 export async function getTemperature(id) {
     const API_KEY = 'aebf87bc58f9c235864c7139afc8000c',
-          URL_BASE = 'http://api.openweathermap.org/data/2.5/weather?'
+          URL_BASE = 'https://api.openweathermap.org/data/2.5/weather?'
 
     const weatherObj = await fetch(`${URL_BASE}id=${id}&units=metric&appid=${API_KEY}`).then(res => res.json())
 
